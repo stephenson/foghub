2c1c2bd160caf16fa3195a607e97df2abd489c02
Simon H. Eskildsen <sirup@sirupsen.com>
2011-05-20 17:40:16 +0200

    Comments on what needs to be done in Sinatra app

--- lib/foghub/foghub.rb
+++ lib/foghub/foghub.rb
@@ -3,6 +3,10 @@ require_relative './parser'
 
 class Foghub < Sinatra::Base
   post '/commit' do
+    # get commit
+    # parse commit for #case and #review
+    # if has #review tag AND >= 1 tagged people, create a code review for #case
+    # else, simply associate the commit with the case
     'hello world'
   end
 end
