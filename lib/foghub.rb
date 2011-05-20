06ca37afe468c635e7be45dea63a0269e2de1990
Simon H. Eskildsen <sirup@sirupsen.com>
2011-05-20 18:23:07 +0200

    Describe in comments what web app should do

--- lib/foghub.rb
+++ lib/foghub.rb
@@ -5,7 +5,9 @@ class Foghub < Sinatra::Base
   post '/commit' do
     # get commit
     # parse commit for #case and #review
-    # if has #review tag AND >= 1 tagged people, create a code review for #case
+    # if has #review tag AND >= 1 tagged people
+    #   find Fogbugz ID from alias
+    #   create a code review for #case assigned to that ID
     # else, simply associate the commit with the case
     'hello world'
   end
