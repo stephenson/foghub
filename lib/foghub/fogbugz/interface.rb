69d93f8c99a68c80ecca6f6b8f87cd12f9cf4e40
Simon H. Eskildsen <sirup@sirupsen.com>
2011-05-20 14:12:28 +0200

    Implement #authenticate and #command in fb interface

--- lib/foghub/fogbugz/interface.rb
+++ lib/foghub/fogbugz/interface.rb
@@ -1,11 +1,25 @@
 module Fogbugz
   class Interface
-    attr_accessor :options, :http, :xml
+    class RequestError < StandardError; end
+    
+    attr_accessor :options, :http, :xml, :token
 
     def initialize(options = {})
       @options = {}.merge(options)
       @http = Fogbugz.adapter[:http].new
       @xml = Fogbugz.adapter[:xml].new
+    end
+
+    def authenticate
+      @token ||= @http.request :logon, { 
+        :email    => @options[:email],
+        :password => @options[:password]
+      }
+    end
+
+    def command(action, parameters)
+      raise RequestError, 'No token available, #authenticate first' unless @token
+      @http.request(action, :token => @token, :params => parameters)
     end
   end
 end
