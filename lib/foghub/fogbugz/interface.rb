6eeb93b91d079dfd4c0ba307c407389d47be103c
Simon H. Eskildsen <sirup@sirupsen.com>
2011-05-20 17:15:21 +0200

    Alias #authenticate to #authenticate! since it alters the object

--- lib/foghub/fogbugz/interface.rb
+++ lib/foghub/fogbugz/interface.rb
@@ -23,6 +23,7 @@ module Fogbugz
 
       @token ||= @xml.parse(response)["token"]
     end
+    alias :authenticate!, :authenticate
 
     def command(action, parameters = {})
       raise RequestError, 'No token available, #authenticate first' unless @token
