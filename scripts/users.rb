1ce65526efaa8bbe18bf21bf956b690509ecaf10
Simon H. Eskildsen <sirup@sirupsen.com>
2011-05-21 12:01:25 +0200

    Added script to list all users and their IDs

---
+++ scripts/users.rb
@@ -0,0 +1,8 @@
+require_relative '../lib/fogbugz/fogbugz'
+
+interface = Fogbugz::Interface.new(YAML.load_file('config.yml')[:fogbugz])
+interface.authenticate
+
+interface.command(:listPeople)["people"]["person"].each do |person|
+  puts "##{person["ixPerson"]} #{person["sFullName"]}"
+end
